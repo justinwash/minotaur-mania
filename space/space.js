9afb73436aa31e492c525e506be0a55b3003d450
Justin Wash <trifectuh@icloud.com>
2018-11-19 21:56:11 -0600

    space shooty movement

---
+++ space/space.js
@@ -0,0 +1,34 @@
+// script: js
+
+var ship = {
+  sp: 0,
+  x: 112,
+  y: 92
+};
+
+function TIC() {
+  cls();
+  _update();
+  _draw();
+}
+
+function _draw() {
+  spr(ship.sp, ship.x, ship.y);
+}
+
+function _update() {
+  moveShip();
+  animatePropulsion();
+}
+
+function animatePropulsion() {
+  if (ship.sp === 0) ship.sp = 1;
+  else ship.sp = 0;
+}
+
+function moveShip() {
+  if (btn(0)) ship.y--;
+  if (btn(1)) ship.y++;
+  if (btn(2)) ship.x--;
+  if (btn(3)) ship.x++;
+}
