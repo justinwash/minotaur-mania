2d955a657e10affd27d6b9b86a64f9208a8d8e7c
justin <justin.wash@pm.me>
2018-11-20 19:53:34 -0600

    ship health, killing aliens

--- jwash/space.js
+++ jwash/space.js
@@ -1,131 +1,146 @@
 // script: js
 
 var ship = {
-  sp: 0,
-  x: 180,
-  y: 120,
-  health: 3,
-  points: 0
+	sp: 0,
+	x: 180,
+	y: 120,
+	health: 3,
+	points: 0
 };
 
 var bullets = [];
 var enemies = initEnemies();
 
 function TIC() {
-  cls();
-  _update();
-  _draw();
+	cls();
+	_update();
+	_draw();
 }
 
 function _draw() {
-  spr(ship.sp, ship.x, ship.y);
-  drawUI();
+	spr(ship.sp, ship.x, ship.y);
+	drawUI();
 }
 
 function _update() {
-  moveShip();
-  animatePropulsion();
-  fire();
-  animateBullets();
-  drawEnemies();
-  moveEnemies();
-  killEnemies();
+	moveShip();
+	animatePropulsion();
+	fire();
+	animateBullets();
+	drawEnemies();
+	moveEnemies();
+	killEnemies();
+	hurtShip();
 }
 
 function animatePropulsion() {
-  if (ship.sp === 0) ship.sp = 1;
-  else ship.sp = 0;
+	if (ship.sp === 0) ship.sp = 1;
+	else ship.sp = 0;
 }
 
 function moveShip() {
-  if (btn(0)) ship.y--;
-  if (btn(1)) ship.y++;
-  if (btn(2)) ship.x--;
-  if (btn(3)) ship.x++;
+	if (btn(0)) ship.y--;
+	if (btn(1)) ship.y++;
+	if (btn(2)) ship.x--;
+	if (btn(3)) ship.x++;
 }
 
 function fire() {
-  if (btnp(4)) {
-    var bullet = {
-      sp: 2,
-      x: ship.x,
-      y: ship.y,
-      dx: 0,
-      dy: -3,
-      active: true
-    };
-    bullets.push(bullet);
-  }
+	if (btnp(4)) {
+		var bullet = {
+			sp: 2,
+			x: ship.x,
+			y: ship.y,
+			dx: 0,
+			dy: -3,
+			active: true
+		};
+		bullets.push(bullet);
+	}
 }
 
 function animateBullets() {
-  for (i = 0; i < bullets.length; i++) {
-    bullets[i].x += bullets[i].dx;
-    bullets[i].y += bullets[i].dy;
-    spr(bullets[i].sp, bullets[i].x, bullets[i].y);
-    if (bullets[i].y < 0) {
-      bullets.splice(bullets[i], 1);
-    }
-  }
+	for (i = 0; i < bullets.length; i++) {
+		bullets[i].x += bullets[i].dx;
+		bullets[i].y += bullets[i].dy;
+		spr(bullets[i].sp, bullets[i].x, bullets[i].y);
+		if (bullets[i].y < 0) {
+			bullets.splice(bullets[i], 1);
+		}
+	}
 }
 
 function drawEnemies() {
-  for (i = 0; i < enemies.length; i++) {
-    spr(enemies[i].sp, enemies[i].x, enemies[i].y);
-  }
+	for (i = 0; i < enemies.length; i++) {
+		spr(enemies[i].sp, enemies[i].x, enemies[i].y);
+	}
 }
 
 function initEnemies() {
-  var e = [];
-  for (i = 0; i < 10; i++) {
-    e.push({
-      sp: 3,
-      alive: true,
-      x: i * Math.random() * (24 + i),
-      y: i * Math.random() * 10
-    });
-  }
-  return e;
+	var e = [];
+	for (i = 0; i < 10; i++) {
+		e.push({
+			sp: 3,
+			alive: true,
+			x: i * Math.random() * (24 + i),
+			y: i * Math.random() * 10
+		});
+	}
+	return e;
 }
 
 function moveEnemies() {
-  for (i = 0; i < 10; i++) {
-    enemies[i].x += 0.2 * Math.sin(enemies[i].y);
-    enemies[i].y += 0.33 * Math.cos(enemies[i].x);
-  }
+	for (i = 0; i < 10; i++) {
+		enemies[i].x += 0.2 * Math.sin(enemies[i].y);
+		enemies[i].y += 0.33 * Math.cos(enemies[i].x);
+	}
 }
 
 function drawUI() {
-  for (i = 1; i <= 4; i++) {
-    if (i <= ship.health) {
-      spr(16, 208 + 6 * i, 3);
-    } else spr(17, 208 + 6 * i, 3);
-  }
-  print(ship.points, 3, 3, 15);
+	for (i = 1; i <= 4; i++) {
+		if (i <= ship.health) {
+			spr(16, 208 + 6 * i, 3);
+		} else spr(17, 208 + 6 * i, 3);
+	}
+	print(ship.points, 3, 3, 15);
 }
 
 function collide(thing1, thing2) {
-  var xDiff = (thing1.x + 100) / (thing2.x + 100);
-  var yDiff = (thing1.y + 100) / (thing2.y + 100);
+	var xDiff = (thing1.x + 100) / (thing2.x + 100);
+	var yDiff = (thing1.y + 100) / (thing2.y + 100);
 
-  if (xDiff >= 0.95 && xDiff <= 1.05 && yDiff >= 0.95 && yDiff <= 1.05) {
-    return true;
-  } else return false;
+	if (xDiff >= 0.95 && xDiff <= 1.05 && yDiff >= 0.95 && yDiff <= 1.05) {
+		return true;
+	} else return false;
 }
 
 function killEnemies() {
-  for (e = 0; e < enemies.length; e++) {
-    for (b = 0; b < bullets.length; b++) {
-      if (collide(enemies[e], bullets[b]) && bullets[b].active) {
-        enemies[e].alive = false;
-        bullets[b].active = false;
-        ship.points++;
-      }
-    }
-  }
+	for (e = 0; e < enemies.length; e++) {
+		for (b = 0; b < bullets.length; b++) {
+			if (
+				collide(enemies[e], bullets[b]) &&
+				bullets[b].active &&
+				enemies[e].alive
+			) {
+				enemies[e].alive = false;
+				enemies[e].sp = 100;
+				bullets[b].active = false;
+				bullets[b].sp = 100;
+				ship.points++;
+			}
+		}
+	}
 }
 
-function hurtShip() {}
+function hurtShip() {
+	for (e = 0; e < enemies.length; e++) {
+		if (collide(enemies[e], ship) && enemies[e].alive) {
+			enemies[e].alive = false;
+			enemies[e].sp = 100;
+			ship.health--;
+		}
+	}
+}
 
 // <TILES>
 // 000:000000000060060000a00a0000aaaa0003a33a3003aaaa3003009030000e0000
