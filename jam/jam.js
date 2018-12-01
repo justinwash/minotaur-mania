581e14c3b4d1e5af58075dc34d49c20f0a25cc72
Alexander Garcini <agarcini@engagesoftware.com>
2018-11-30 21:18:09 -0600

    moving sprite from bg to fg

--- jam/jam.js
+++ jam/jam.js
@@ -16,9 +16,9 @@ function TIC() {
 	spr(0, x, y, 14, 3, 0, 0, 1, 1);
 }
 
-// <TILES>
+// <SPRITES>
 // 000:000f00f0001111f0006f1ff00011114000114110014444100043440001111100
-// </TILES>
+// </SPRITES>
 
 // <WAVES>
 // 000:00000000ffffffff00000000ffffffff
@@ -33,3 +33,4 @@ function TIC() {
 // <PALETTE>
 // 000:140c1c44243430346d4e4a4e854c30346524d04648757161597dced27d2c8595a16daa2cd2aa996dc2cadad45edeeed6
 // </PALETTE>
+
